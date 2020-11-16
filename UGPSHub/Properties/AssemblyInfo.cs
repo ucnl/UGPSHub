39967103f70e40860e44d9a32dd639c740181e3b
Aleksandr <aleksandr.v.dikarev@gmail.com>
2020-11-16 14:36:01 +0400

    Create AssemblyInfo.cs

---
+++ UGPSHub/Properties/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UGPSHub")]
+[assembly: AssemblyDescription("RedWAVE/DiveNET GPS - Underwater GPS host application.\n\n" +
+                               "Latest release: https://api.github.com/repos/ucnl/UGPSHub/zipball \n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "More information about RedWAVE/DiveNET GPS Underwater GPS:\n" +
+                               "English: https://docs.unavlab.com/navigation_and_tracking_systems_en.html#redwave\n" +
+                               "Russian: https://docs.unavlab.com/navigation_and_tracking_systems_ru.html#redwave\n\n" +
+                               "Project source code:\n" +
+                               "https://github.com/ucnl/UGPSHub\n\n" +
+                               "This application is 100% open-source, and based on our projects:\n" +                               
+                               "https://github.com/ucnl/UCNLNav\n" +
+                               "https://github.com/ucnl/UCNLPhysics\n" +
+                               "https://github.com/ucnl/UCNLNMEA\n" +
+                               "https://github.com/ucnl/UCNLKML\n" +
+                               "https://github.com/ucnl/UCNLDrivers\n" +
+                               "https://github.com/ucnl/UCNLUI"
+                               )]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("UGPSHub")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, 2020")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72503d5d-3aeb-44a5-bcdc-81bce06e25bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
